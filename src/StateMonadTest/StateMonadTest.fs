1ab6f8f99ffcc086cd0980afc935ff196987b537
Arialdo Martini <arialdo.martini@gmail.com>
2024-07-02 12:21:51 +0200

    Counts the number of leaves of a tree

--- src/StateMonadTest/StateMonadTest.fs
+++ src/StateMonadTest/StateMonadTest.fs
@@ -3,6 +3,31 @@ module StateMonadTest
 open Xunit
 open Swensen.Unquote
 
+//     Node
+//     /  \
+//  Leaf  Leaf
+
+//     Node
+//     /  \
+//  Leaf  Node
+//        /  \
+//    Leaf   Leaf
+
+//       Leaf
+
+
+type Tree =
+    | Leaf
+    | Node of Tree * Tree
+
+let rec numberOfLeaves = function
+    | Leaf -> 1
+    | Node(l, r) -> numberOfLeaves l + numberOfLeaves r
+
 [<Fact>]
-let ``dummy test`` () =
-    test <@ true = true @>
+let ``counts the number of leaves in a tree`` () =
+    let tree = Node(Leaf, Node(Leaf, Leaf))
+
+    let leaves = numberOfLeaves tree
+
+    test <@ leaves = 3 @>
