a18c1d8d4de9a2b631ac2fc4c01845f40bbcc41f
Arialdo Martini <arialdo.martini@gmail.com>
2024-07-02 13:03:32 +0200

    Calculates the length of leaves' content

--- src/StateMonadTest/StateMonadTest.fs
+++ src/StateMonadTest/StateMonadTest.fs
@@ -16,18 +16,33 @@ open Swensen.Unquote
 //       Leaf
 
 
-type Tree =
-    | Leaf
-    | Node of Tree * Tree
+type Tree<'a> =
+    | Leaf of 'a
+    | Node of Tree<'a> * Tree<'a>
 
+// Tree a -> Int
 let rec numberOfLeaves = function
-    | Leaf -> 1
+    | Leaf _ -> 1
     | Node(l, r) -> numberOfLeaves l + numberOfLeaves r
+    
+// Tree String -> Tree Int    
+let rec lengths = function
+    | Leaf v -> Leaf (String.length v)
+    | Node(l, r) -> Node (lengths l, lengths r)
 
 [<Fact>]
 let ``counts the number of leaves in a tree`` () =
-    let tree = Node(Leaf, Node(Leaf, Leaf))
+    let tree = Node(Leaf (), Node(Leaf (), Leaf ()))
 
     let leaves = numberOfLeaves tree
 
     test <@ leaves = 3 @>
+
+[<Fact>]
+let ``counts the leaves' content length`` () =
+    let treeOfWords = Node(Leaf "one", Node(Leaf "two", Leaf "three"))
+    let treeOfNumbers = Node(Leaf 3, Node(Leaf 3, Leaf 5))
+
+    let treeOfLengths = lengths treeOfWords
+
+    test <@ treeOfLengths = treeOfNumbers @>
