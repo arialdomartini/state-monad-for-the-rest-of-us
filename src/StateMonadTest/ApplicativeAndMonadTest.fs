7873833422bd553252c9a8353ef9021f5f93865c
Arialdo Martini <arialdo.martini@gmail.com>
2024-07-13 16:57:29 +0200

    Writing Chapter 9

--- src/StateMonadTest/ApplicativeAndMonadTest.fs
+++ src/StateMonadTest/ApplicativeAndMonadTest.fs
@@ -4,36 +4,60 @@ open Xunit
 open Swensen.Unquote
 open Tree
 
-let mutable count = 1
-let rec imperativeIndex =
-    function
-    | Leaf v ->
-        let indexedLeaf = Leaf (v, count)
-        count <- count + 1
-        indexedLeaf
-    | Node(l, r) -> Node(imperativeIndex l, imperativeIndex r)
-
 let tree = Node(Leaf "one", Node(Leaf "two", Leaf "three"))
 
+//type WithCount<'v> = WithCount of (int -> Tree<'v * int> * int)
+//type WithCount = WithCount of (int -> Tree<string * int> * int)
+type WithCount<'v> = WithCount of (int -> 'v * int)
+//type WithCount<'s, 'v> = WithCount of ('s -> 'v * 's)
+//type WithCount<'f> = WithCount of 'f
 
-[<Fact>]
-let ``impurely indexes a tree`` () =
-    let indexed = imperativeIndex tree
 
-    test <@ indexed = Node(Leaf ("one", 1), Node(Leaf ("two", 2), Leaf ("three", 3))) @>
+let build l r = Node(l, r)
+let run (WithCount f) (count: int)= f count
+ 
+let add x y = x + y
 
-// Tree a -> (Int -> (Tree (a, Int), Int))
-let rec index =
+let (<|) f a = f a 
+let r = add <| 2 <| 3
+
+let pure'<'v> (v: 'v) : WithCount<'v> = WithCount (fun count -> (v, count))
+
+// WithCount (a -> b) -> WithCount a -> WithCount b 
+let (<*>) f a = 
+    WithCount (fun count ->
+        let fv, fc = run f count
+        let av, ac = run a fc
+        let b = fv av
+        b, ac)
+
+let incCount = WithCount (fun c -> (), c + 1)
+let putCount c = WithCount (fun _ -> (), c)
+let getCount = WithCount (fun c -> c, c)
+//
+// let incCount =
+//     let f get put =
+//         let count = get
+//         let _ = put (count + 1)
+//         ()
+//     pure' f <*> getCount <*> putCount
+
+let buildLeaf v count put =
+    let leaf = Leaf (v, count)
+    put (count + 1) |> ignore
+    leaf
+
+let rec index<'a> =
     function
-    | Leaf v -> fun count -> (Leaf (v, count), count + 1)
-    | Node (l, r) ->
-        fun count ->
-            let li, lc = index l count
-            let ri, rc = index r lc
-            Node (li, ri), rc
+    | Leaf v ->
+        // pure' buildLeaf <*> (pure' v) <*> getCount <*> (pure' putCount)
+        pure' buildLeaf <*> (pure' v) <*> getCount <*> pure' putCount
+    | Node(l, r) ->
+        pure' build <*> index l <*> index r
 
 [<Fact>]
 let ``indexes a tree`` () =
-    let indexed, _ = index tree 1
+    let withCount = index tree
+    let indexed, _ = run withCount 1
 
-    test <@ indexed = Node(Leaf ("one", 1), Node(Leaf ("two", 2), Leaf ("three", 3))) @>
+    test <@ indexed = Node(Leaf("one", 1), Node(Leaf("two", 2), Leaf("three", 3))) @>
---
+++ src/StateMonadTest/PurelyIndexing.fs
@@ -0,0 +1,39 @@
+module StateMonadTest.PurelyIndexing
+
+open Xunit
+open Swensen.Unquote
+open Tree
+
+let mutable count = 1
+let rec imperativeIndex =
+    function
+    | Leaf v ->
+        let indexedLeaf = Leaf (v, count)
+        count <- count + 1
+        indexedLeaf
+    | Node(l, r) -> Node(imperativeIndex l, imperativeIndex r)
+
+let tree = Node(Leaf "one", Node(Leaf "two", Leaf "three"))
+
+
+[<Fact>]
+let ``impurely indexes a tree`` () =
+    let indexed = imperativeIndex tree
+
+    test <@ indexed = Node(Leaf ("one", 1), Node(Leaf ("two", 2), Leaf ("three", 3))) @>
+
+// Tree a -> (Int -> (Tree (a, Int), Int))
+let rec index =
+    function
+    | Leaf v -> fun count -> (Leaf (v, count), count + 1)
+    | Node (l, r) ->
+        fun count ->
+            let li, lc = index l count
+            let ri, rc = index r lc
+            Node (li, ri), rc
+
+[<Fact>]
+let ``indexes a tree`` () =
+    let indexed, _ = index tree 1
+
+    test <@ indexed = Node(Leaf ("one", 1), Node(Leaf ("two", 2), Leaf ("three", 3))) @>
