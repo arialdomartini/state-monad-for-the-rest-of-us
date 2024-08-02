111a6a2396b2925767c2e79228a047f03fca6c42
Arialdo Martini <arialdo.martini@gmail.com>
2024-08-02 07:25:14 +0200

    Alternative implementations for pop and push

--- src/StateMonadTest/StateMonad/StackTest.fs
+++ src/StateMonadTest/StateMonad/StackTest.fs
@@ -6,6 +6,8 @@ open StateMonadTest.StateMonad.StateMonad
 
 type Stack<'s, 'a> = State<'s list, 'a>
 
+
+
 let pop =
     state {
         let! stack = get
@@ -13,6 +15,7 @@ let pop =
         do! put t
         return h
     }
+
 
 let push (v: 'v) : State<'v list, unit> =
     state {
@@ -80,3 +83,47 @@ let ``uses a LIFO stack`` () =
     test <@ result1 = expected @>
     test <@ result2 = expected @>
     test <@ result3 = expected @>
+     
+
+// Alternative implementations
+let push' v =
+     State (fun s -> ((), v::s))
+
+let pop' =
+    State (fun s -> (List.head s, List.tail s))
+    
+[<Fact>]
+let ``alternative implementations`` () =
+    let eval input =
+        if input |> isInteger then
+            state {
+                do! push' input
+                return input
+            }
+        else
+            let op = ops[input]
+
+            state {
+                let! a = pop'
+                let! b = pop'
+                let result = op (b |> asInteger) (a |> asInteger)
+                do! push' (result.ToString())
+                return result.ToString()
+            }
+
+    let expected = ((2 + 3) * (5 * (50 - 40))).ToString()
+
+    let r1 =
+        eval "2"
+        >> eval "3"
+        >> eval "+"
+        >> eval "5"
+        >> eval "50"
+        >> eval "40"
+        >> eval "-"
+        >> eval "*"
+        >> eval "*"
+
+    let result1, _ = run r1 []
+    
+    test <@ result1 = expected @>
